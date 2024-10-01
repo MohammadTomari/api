4da529dcbf4774830d72f5cc297ea511991509c8
Mohammad Tomari <contact.mtomari@gmail.com>
2024-10-02 01:04:43 +0330

    change AppServiceProvider.php & User model

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,23 +3,15 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+    public function register(): void {}
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        Passport::hashClientSecrets();
+        Schema::defaultStringLength(191);
     }
 }
